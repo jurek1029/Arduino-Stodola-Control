c4c421147e1abb66e68895be368a365d793110c5
KubaJurek <kuba@cherrypickgames.com>
2022-02-03 22:42:59 +0100

    WebSocket Servre

--- SimpleAuthentication.ino
+++ SimpleAuthentication.ino
@@ -31,7 +31,7 @@ String heatingState = "OFF";
 
 DHT dht(PIN_DTH, DHTTYPE);
 AsyncWebServer server(80);
-
+AsyncWebSocket ws("http://127.0.0.1:8081/");
 WiFiManager wifiManager;
 
 //Check if header is present and correct
@@ -180,43 +180,6 @@ void setupWebServer(){
   Serial.println("HTTP server started");
 }
 
-/*
-void setupOTA(){
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else { // U_FS
-      type = "filesystem";
-    }
-
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
-    }
-  });
-  ArduinoOTA.begin();
-}
-
-*/
 
 void setup(void) {
   Serial.begin(115200);
@@ -229,7 +192,6 @@ void setup(void) {
   dht.begin();
 
   setupWiFi();
-  //setupOTA();
   AsyncElegantOTA.begin(&server);    // Start ElegantOTA
   setupWebServer();
 
@@ -240,7 +202,6 @@ void setup(void) {
 }
 
 void loop(void) {
- // ArduinoOTA.handle();
   if((millis() - lastDTHRead) > DTH_READ_DELAY){
     h = dht.readHumidity();
     t = dht.readTemperature();
